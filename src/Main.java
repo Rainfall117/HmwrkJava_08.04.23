da85f5eda87d65839ecc45ac98356b3108d125ce
Rain <drophea117@gmail.com>
2023-04-11 12:50:54 +0600

    newCommit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7500;
+        int bonus20 = 20;
+        int miles = ticketPrice / bonus20;
+        System.out.println(miles);
+    }
+}
